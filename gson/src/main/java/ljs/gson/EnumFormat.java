6083e1f624c0b98a8b30809dfc4bcf7701cef06b
liujiangshan <1015905004@qq.com>
2018-06-03 22:39:29 +0800

    枚举对象通用序列号与反序列化配置

---
+++ gson/src/main/java/ljs/gson/EnumFormat.java
@@ -0,0 +1,47 @@
+package ljs.gson;
+
+import com.google.gson.*;
+import ljs.exception.KnowException;
+import ljs.reflect.ClassUtils;
+import ljs.reflect.EnumUtils;
+import ljs.reflect.FieldUtils;
+
+import java.lang.reflect.Type;
+
+public class EnumFormat<T> implements JsonDeserializer<T>, JsonSerializer<T> {
+
+    private Class<T> type;
+
+    private String valueFieldName;
+
+    private JsonToValue jsonToValue;
+
+    interface JsonToValue {
+        Object get(JsonElement json);
+    }
+
+    public EnumFormat(JsonToValue jsonToValue) {
+        this(EnumUtils.DefaultValueFieldName, jsonToValue);
+    }
+
+    public EnumFormat(String valueFieldName, JsonToValue jsonToValue) {
+        type = (Class<T>) ClassUtils.getGenericClassByObj(this)[0];
+        this.valueFieldName = valueFieldName;
+        this.jsonToValue = jsonToValue;
+    }
+
+    @Override
+    public T deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        try {
+            return EnumUtils.parse(jsonToValue.get(json), type);
+        } catch (KnowException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public JsonElement serialize(T src, Type typeOfSrc, JsonSerializationContext context) {
+        int value = (int) FieldUtils.getValueByName(src, valueFieldName);
+        return src == null ? null : new JsonPrimitive(value);
+    }
+}
